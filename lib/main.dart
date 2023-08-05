f6ab057217a83c3ec0e9c89fb8221bf16e1129cd
Chandra <dchandrachaan@gmail.com>
2023-08-05 21:29:42 +0530

    basic files done

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,276 @@
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(TodoApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TODO app',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TodoList(),
     );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Task> tasks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  void _loadTasks() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/tasks.txt');
+    if (file.existsSync()) {
+      setState(() {
+        tasks = Task.fromFileString(file.readAsStringSync());
+      });
+    }
+  }
+
+  void _saveTasks() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/tasks.txt');
+    file.writeAsStringSync(Task.toFileString(tasks));
+  }
+
+  void _addTask(Task task) {
+    setState(() {
+      tasks.add(task);
+    });
+    _saveTasks();
+  }
+
+  void _editTask(int index, Task task) {
+    setState(() {
+      tasks[index] = task;
+    });
+    _saveTasks();
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      tasks.removeAt(index);
+    });
+    _saveTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Todo List')),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index].title),
+            subtitle: Text(tasks[index].description),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => EditTaskScreen(
+                          task: tasks[index],
+                          onSave: (editedTask) => _editTask(index, editedTask),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () => _removeTask(index),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddTaskScreen(
+                onSave: (newTask) => _addTask(newTask),
+              ),
+            ),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddTaskScreen extends StatefulWidget {
+  final Function(Task) onSave;
+
+  AddTaskScreen({required this.onSave});
+
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Task')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String title = _titleController.text;
+                String description = _descriptionController.text;
+                if (title.isNotEmpty) {
+                  widget.onSave(Task(title: title, description: description));
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditTaskScreen extends StatefulWidget {
+  final Task task;
+  final Function(Task) onSave;
+
+  EditTaskScreen({required this.task, required this.onSave});
+
+  @override
+  _EditTaskScreenState createState() => _EditTaskScreenState();
+}
+
+class _EditTaskScreenState extends State<EditTaskScreen> {
+  late TextEditingController _titleController;
+  late TextEditingController _descriptionController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.task.title);
+    _descriptionController = TextEditingController(text: widget.task.description);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit Task')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                String title = _titleController.text;
+                String description = _descriptionController.text;
+                if (title.isNotEmpty) {
+                  widget.onSave(Task(title: title, description: description));
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Task {
+  String title;
+  String description;
+
+  Task({required this.title, required this.description});
+
+  static List<Task> fromFileString(String fileString) {
+    List<Task> tasks = [];
+    List<String> lines = fileString.split('\n');
+    for (var line in lines) {
+      if (line.isNotEmpty) {
+        var parts = line.split(',');
+        if (parts.length >= 2) {
+          tasks.add(Task(title: parts[0], description: parts[1]));
+        }
+      }
+    }
+    return tasks;
+  }
+
+  static String toFileString(List<Task> tasks) {
+    String fileString = '';
+    for (var task in tasks) {
+      fileString += '${task.title},${task.description}\n';
+    }
+    return fileString;
   }
 }
